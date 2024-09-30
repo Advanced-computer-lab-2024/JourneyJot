94afcfa21831e23cb17d70d1f88f880c379506a6
gando1354 <132471558+gando1354@users.noreply.github.com>
2024-09-30 14:15:05 +0300

    finished req id 19

--- backend/App.js
+++ backend/App.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-require("dotenv").config();
-const productRoutes = require("./routes/Products");
+/** @format */
+
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+require('dotenv').config();
+const productRoutes = require('./routes/Products');
+const advertiserRoutes = require('./routes/Advertisers');
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use("/products", productRoutes);
+app.use('/products', productRoutes);
+app.use('/advertisers', advertiserRoutes);
 
 mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("MongoDB is now connected!");
-    // Starting server
-    app.listen(PORT, () => {
-      console.log(`Listening to requests on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+	.connect(MONGO_URI)
+	.then(() => {
+		console.log('MongoDB is now connected!');
+		// Starting server
+		app.listen(PORT, () => {
+			console.log(`Listening to requests on http://localhost:${PORT}`);
+		});
+	})
+	.catch((err) => console.log(err));
--- backend/routes/Products.js
+++ backend/routes/Products.js
@@ -1,56 +1,58 @@
-const express = require("express");
+/** @format */
+
+const express = require('express');
 const router = express.Router();
-const Product = require("../models/Product");
+const Product = require('../models/Product');
 
 // posting a new product
-router.post("/", async (req, res) => {
-  try {
-    const product = new Product({
-      details: req.body.details,
-      price: req.body.price,
-      quantity: req.body.quantity,
-      rating: req.body.rating,
-    });
-    await product.save();
-    res.status(201).json({
-      message: "Product created successfully",
-      product,
-    });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+router.post('/', async (req, res) => {
+	try {
+		const product = new Product({
+			details: req.body.details,
+			price: req.body.price,
+			quantity: req.body.quantity,
+			rating: req.body.rating,
+		});
+		await product.save();
+		res.status(201).json({
+			message: 'Product created successfully',
+			product,
+		});
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
 });
 
 // getting all products sorted by rating
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find().sort({ rating: 1 });
-    res.status(200).json({ products });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+router.get('/', async (req, res) => {
+	try {
+		const products = await Product.find().sort({ rating: 1 });
+		res.status(200).json({ products });
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
 });
 
 // eddting a product by id
-router.put("/:id", async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      // checking if details was passed in the request
-      if (req.body.details) product.details = req.body.details;
-      // checking if price was passed in the request
-      if (req.body.price) product.price = req.body.price;
-      await product.save();
-      res.status(200).json({
-        message: "Product updated successfully",
-        product,
-      });
-    } else {
-      res.status(404).json({ message: "Product not found" });
-    }
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+router.put('/:id', async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		if (product) {
+			// checking if details was passed in the request
+			if (req.body.details) product.details = req.body.details;
+			// checking if price was passed in the request
+			if (req.body.price) product.price = req.body.price;
+			await product.save();
+			res.status(200).json({
+				message: 'Product updated successfully',
+				product,
+			});
+		} else {
+			res.status(404).json({ message: 'Product not found' });
+		}
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
 });
 
 module.exports = router;
