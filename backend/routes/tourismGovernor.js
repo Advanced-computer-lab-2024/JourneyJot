ab2d0a6734d9197244008ea6c26e877a0197a3b5
gando1354 <132471558+gando1354@users.noreply.github.com>
2024-10-03 21:06:40 +0300

    hey

--- backend/routes/tourismGovernor.js
+++ backend/routes/tourismGovernor.js
@@ -1,12 +1,22 @@
+/** @format */
+
 const express = require('express');
 const router = express.Router();
 const tourismGovernorController = require('../controllers/tourismGovernorController');
 const { authenticateUser } = require('../middleware/authMiddleware');
 
 // Route to get all museums created by the logged-in Tourism Governor
-router.get('/museums', authenticateUser, tourismGovernorController.getMyMuseums);
+router.get(
+	'/museums',
+	authenticateUser,
+	tourismGovernorController.getMyMuseums
+);
 
 // Route to get all historical places created by the logged-in Tourism Governor
-router.get('/historical-places', authenticateUser, tourismGovernorController.getMyHistoricalPlaces);
+router.get(
+	'/historical-places',
+	authenticateUser,
+	tourismGovernorController.getMyHistoricalPlaces
+);
 
 module.exports = router;
