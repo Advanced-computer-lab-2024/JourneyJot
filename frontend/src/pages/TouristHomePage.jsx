c4ae2ca60a606ff62be19fef67eccbb0f3c0ed2c
mohamedddAli <moamedamr584@gmail.com>
2024-10-08 00:19:38 +0300

    added product path to admin and tourist

--- frontend/src/pages/TouristHomePage.jsx
+++ frontend/src/pages/TouristHomePage.jsx
@@ -9,6 +9,11 @@ const TouristHomePage = () => {
           My Profile
         </button>
       </Link>
+      <Link to={"products"}>
+        <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow hover:bg-blue-700 transition duration-200">
+          Products
+        </button>
+      </Link>
     </div>
   );
 };
