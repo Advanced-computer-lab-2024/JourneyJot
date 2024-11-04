86d94f4ecb244e5ae8dc77b5ec87db469c67844e
gando1354 <132471558+gando1354@users.noreply.github.com>
2024-11-04 18:30:52 +0200

    gandoo

--- frontend/src/pages/Advertiser/ActivitiesPage.jsx
+++ frontend/src/pages/Advertiser/ActivitiesPage.jsx
@@ -1,41 +1,42 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import ActivitiesCard from "../../components/Advertiser/ActivitiesCard";
+/** @format */
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import ActivitiesCard from '../../components/Advertiser/ActivitiesCard';
 
 const ActivitiesPage = () => {
-  const [activities, setActivities] = useState([]);
+	const [activities, setActivities] = useState([]);
 
-  const fetchActivities = async () => {
-    try {
-      const response = await axios.get("http://localhost:3000/activities");
-      console.log(response.data.activities);
-      setActivities(response.data.activities);
-    } catch (error) {
-      console.error("Error fetching activities:", error);
-    }
-  };
+	const fetchActivities = async () => {
+		try {
+			const response = await axios.get('http://localhost:3000/activities');
+			console.log(response.data.activities);
+			setActivities(response.data.activities);
+		} catch (error) {
+			console.error('Error fetching activities:', error);
+		}
+	};
 
-  useEffect(() => {
-    fetchActivities();
-  }, []);
+	useEffect(() => {
+		fetchActivities();
+	}, []);
 
-  const handleDelete = (activityId) => {
-    setActivities((prevActivities) =>
-      prevActivities.filter((activity) => activity._id !== activityId)
-    );
-  };
+	const handleDelete = (activityId) => {
+		setActivities((prevActivities) =>
+			prevActivities.filter((activity) => activity._id !== activityId)
+		);
+	};
 
-  return (
-    <div>
-      <ActivitiesCard
-        activities={activities}
-        isAdvertiser={true}
-        onDelete={handleDelete}
-        fetchActivities={fetchActivities}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<ActivitiesCard
+				activities={activities}
+				isAdvertiser={true}
+				onDelete={handleDelete}
+				fetchActivities={fetchActivities}
+			/>
+		</div>
+	);
 };
 
 export default ActivitiesPage;
