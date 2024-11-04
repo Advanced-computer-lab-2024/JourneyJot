56ec95c99878587319c416344ab389e27c02a7eb
gando1354 <132471558+gando1354@users.noreply.github.com>
2024-11-04 18:27:18 +0200

    finishedd 2 and 10

--- backend/routes/attraction.js
+++ backend/routes/attraction.js
@@ -2,26 +2,25 @@
 
 // routes/attractionRoutes.js
 
-const express = require("express");
+const express = require('express');
 const {
-  createAttraction,
-  getAttractions,
-  updateAttraction,
-  deleteAttraction,
-  getAttraction,
-  filterAttractionsByTag,
-} = require("../controllers/attractions");
-const authenticate = require("../middleware/auth-check"); // Assuming you have an auth middleware
-const roleCheck = require("../middleware/governor-check"); // Check if user is a tourism governor
-const viewList = require("../middleware/view-list");
+	createAttraction,
+	getAttractions,
+	updateAttraction,
+	deleteAttraction,
+	getAttraction,
+	filterAttractionsByTag,
+} = require('../controllers/attractions');
+const authenticate = require('../middleware/auth-check'); // Assuming you have an auth middleware
+const roleCheck = require('../middleware/governor-check'); // Check if user is a tourism governor
 
 const attractionRouter = express.Router();
 
-attractionRouter.post("/", createAttraction); // Create attraction
-attractionRouter.get("/", getAttractions); // Get all attractions
-attractionRouter.get("/filter", filterAttractionsByTag); // Filter attractions by tags
-attractionRouter.get("/:id", getAttraction); // Get one attractions
-attractionRouter.put("/:id", updateAttraction); // Update attraction
-attractionRouter.delete("/:id", deleteAttraction); // Delete attraction
+attractionRouter.post('/', createAttraction); // Create attraction
+attractionRouter.get('/', getAttractions); // Get all attractions
+attractionRouter.get('/filter', filterAttractionsByTag); // Filter attractions by tags
+attractionRouter.get('/:id', getAttraction); // Get one attractions
+attractionRouter.put('/:id', updateAttraction); // Update attraction
+attractionRouter.delete('/:id', deleteAttraction); // Delete attraction
 
 module.exports = attractionRouter;
