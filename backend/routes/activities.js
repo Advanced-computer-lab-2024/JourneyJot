ee8216717b3deec0b10e422bf4a469bf68705c16
kareemkr <kareemradwan09@gmail.com>
2024-11-09 18:10:51 +0200

    kr

--- backend/routes/activities.js
+++ backend/routes/activities.js
@@ -2,28 +2,28 @@
 
 // routes/activityRoutes.js
 
-const express = require('express');
+const express = require("express");
 const {
-	createActivity,
-	getActivities,
-	updateActivity,
-	deleteActivity,
-	getActivity,
-	getFilteredActivities,
-	sortByPriceOrRating,
-	getCompletedActivities,
-} = require('../controllers/activity');
-const authenticate = require('../middleware/auth-check'); // Assuming you have an auth middleware
-const advertiserCheck = require('../middleware/advertiser-check');
+  createActivity,
+  getActivities,
+  updateActivity,
+  deleteActivity,
+  getActivity,
+  getFilteredActivities,
+  sortByPriceOrRating,
+  getCompletedActivities,
+} = require("../controllers/activity");
+const authenticate = require("../middleware/auth-check"); // Assuming you have an auth middleware
+const advertiserCheck = require("../middleware/advertiser-check");
 const activityRouter = express.Router();
 
-activityRouter.post('/', authenticate, advertiserCheck, createActivity); // Create activity
-activityRouter.get('/', getActivities); // Get all activities
-activityRouter.put('/:id', authenticate, advertiserCheck, updateActivity); // Update activity
-activityRouter.delete('/:id', authenticate, advertiserCheck, deleteActivity); // Delete activity
-activityRouter.get('/filter', getFilteredActivities); // Filter activities
-activityRouter.get('/sort', sortByPriceOrRating); // Sort activities
-activityRouter.get('/:id', getActivity); // Get single activity by id
-activityRouter.get('/complete', getCompletedActivities);
+activityRouter.post("/", authenticate, advertiserCheck, createActivity); // Create activity
+activityRouter.get("/", getActivities); // Get all activities
+activityRouter.put("/:id", authenticate, advertiserCheck, updateActivity); // Update activity
+activityRouter.delete("/:id", authenticate, advertiserCheck, deleteActivity); // Delete activity
+activityRouter.get("/filter", getFilteredActivities); // Filter activities
+activityRouter.get("/sort", sortByPriceOrRating); // Sort activities
+activityRouter.get("/:id", getActivity); // Get single activity by id
+activityRouter.get("/kr/:id", getCompletedActivities);
 
 module.exports = activityRouter;
